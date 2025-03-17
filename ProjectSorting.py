8ce0fae9ee653a0aa7fe87c045002ae795ef15ba
Kapcpa <kacpa2006@gmail.com>
2025-03-17 11:24:45 +0100

    vscode project setup, other
    working on implementations of the sorting algorithms
    
    generated benchmark arrays

---
+++ ProjectSorting.py
@@ -0,0 +1,78 @@
+"""
+TODO: 
+
+quick sorts, shell sort, 
+
+creating arrays of numbers, 
+
+timing functions / benchmark, 
+
+plotting the complexity graphs, 
+
+make the report
+
+""" 
+
+import random
+
+
+### ARRAY GENERATION
+
+
+def make_random_array(n, min_number: int = 0, max_number: int = 100):
+    return [random.randint(min_number, max_number) for _ in range(n)]
+
+
+def make_descending_array(n, min_number: int = 0, max_number: int = 100):
+    return sorted(make_random_array(n, min_number, max_number), reverse=True)
+
+
+def make_ascending_array(n, min_number: int = 0, max_number: int = 100):
+    return sorted(make_random_array(n, min_number, max_number), reverse=False)
+
+
+### SORTING ALGORITHMS IMPLEMENTATION
+
+
+def insertion_sort(array):
+    for i in range(1, len(array)):  
+        key = array[i]
+        j = i - 1
+        while j >= 0 and array[j] > key:  
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = key
+
+
+def selection_sort(array):
+    for i in range(len(array)):
+        min_index = i
+        for j in range(i + 1, len(array)):
+            if array[j] < array[min_index]:  
+                min_index = j  
+        array[i], array[min_index] = array[min_index], array[i]  
+
+
+def make_heap(array, n, i):
+    largest = i
+    left = 2 * i + 1  
+    right = 2 * i + 2  
+
+    if left < n and array[left] > array[largest]:
+        largest = left
+
+    if right < n and array[right] > array[largest]:
+        largest = right
+
+    if largest != i:
+        array[i], array[largest] = array[largest], array[i]
+        make_heap(array, n, largest)
+
+
+def heap_sort(array):
+    for i in range(len(array) // 2 - 1, -1, -1):
+        make_heap(array, len(array), i)
+
+    for i in range(len(array) - 1, 0, -1):
+        array[i], array[0] = array[0], array[i]
+        make_heap(array, i, 0)
--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-"""
-To run this file issue this command:
-python3 HelloWorld.py
+from ProjectSorting import *
 
-Python as a interpreted language executes code directly without a separate compilation step, 
-translating and running the source code on-the-fly during execution.
-"""
-
-from HelloWorld import HelloWorld
-
-# Checks if the Python script is being run as the main program (not imported as a module)
 if __name__ == "__main__":
-    print(HelloWorld())
+    array = make_descending_array(100)
+    heap_sort(array)
+    print("array:", array)
     
